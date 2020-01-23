2dc253d14a3737888501cf83cfc09bcfca3a582f
dammyololade <dammyololade2010@gmail.com>
2020-01-23 13:35:12 +0300

    changed to bloc implementation to use rxdart

--- lib/src/ui/UIListView.dart
+++ lib/src/ui/UIListView.dart
@@ -1,8 +1,9 @@
 import 'package:code_brew/src/bloc/BaseBloc.dart';
 import 'package:code_brew/src/models/BaseModel.dart';
+import 'package:code_brew/src/models/BlocModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
+import '../bloc/BaseBloc.dart';
 
 
 ///A wonder ful
@@ -28,82 +29,38 @@ class _UIListViewState<T> extends State<UIListView> {
 
   BaseBloc baseBloc;
   // T todoModel;
-  RefreshController _refreshController = RefreshController();
 
   @override
   void initState() {
-//    baseBloc = BlocProvider.of<BaseBloc>(context);
-//    baseBloc.modellable = widget.model; // TodoModel();
-//    baseBloc.add(BaseEvent.loadData());
+    baseBloc = BaseBloc(widget.model);
+    baseBloc.add(BlocEvent.fetch);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<BaseBloc, BaseState>(
+    return StreamBuilder<BlocModel>(
+      stream: baseBloc.outBlocModel,
       // ignore: missing_return
-        builder: (context, state) {
-          return state.when(
-              initial: (_) =>
-                  Center(
-                    child: CircularProgressIndicator(),
-                  ),
-              loading: (_) =>
-                  Center(
-                    child: CircularProgressIndicator(),
-                  ),
-              loaded: (state) {
-                print("data is back");
-                print(state.data);
-                if(state.data is T) {
-                  // todoModel = state.todo;
-                  return SmartRefresher(controller: _refreshController,
-                    enablePullDown: true,
-                    enablePullUp: false,
-                    header: WaterDropHeader(),
-                    footer: CustomFooter(
-                      builder: (BuildContext context,LoadStatus mode){
-                        Widget body ;
-                        if(mode==LoadStatus.idle){
-                          body =  Text("pull up load");
-                        }
-                        else if(mode==LoadStatus.loading){
-                          body =  CupertinoActivityIndicator();
-                        }
-                        else if(mode == LoadStatus.failed){
-                          body = Text("Load Failed!Click retry!");
-                        }
-                        else if(mode == LoadStatus.canLoading){
-                          body = Text("release to load more");
-                        }
-                        else{
-                          body = Text("No more Data");
-                        }
-                        return Container(
-                          height: 55.0,
-                          child: Center(child:body),
-                        );
-                      },
-                    ),
-                    onRefresh: () => baseBloc.add(BaseEvent.refresh()),
-                    child: ListView.builder(
-                        padding: EdgeInsets.zero,
-                        itemCount: getItemCount(state.data), // todoModel.todoList.length,
-                        itemBuilder: (context, index) {
-                          return widget.itemBuilder(context, index, state.data); // _buildItem(todoModel.todoList[index]);
-                        }
-                    ),
-                  );
-                } else {
-                  return Text("cant identify this model");
-                }
-              },
-              error: (e) => Text("Error ${e.message}"),
-              dataRefreshed: (DataRefreshed ) {
+        builder: (context, AsyncSnapshot<BlocModel> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                      child: CircularProgressIndicator(),
+                    );
+            case ConnectionState.active:
+            case ConnectionState.done:
 
-              }
-          );
-        }
+              return ListView.builder(
+                        padding: EdgeInsets.zero,
+                        itemCount: getItemCount(snapshot.data.data), // todoModel.todoList.length,
+                        itemBuilder: (context, index) {
+                          return widget.itemBuilder(context, index, snapshot.data.data); // _buildItem(todoModel.todoList[index]);
+                        }
+                    );
+          }
+      }
     );
   }
 
@@ -114,6 +71,12 @@ class _UIListViewState<T> extends State<UIListView> {
     }
 //    if (data is Map)
     return 0;
+  }
+
+  @override
+  void dispose() {
+    baseBloc.dispose();
+    super.dispose();
   }
 
 }
