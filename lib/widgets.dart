aaca821774b6248cb90242d1f41fb8fbf862ac62
MadeFix <oladosulekan@yahoo.com>
2020-04-19 09:21:44 +0100

    - Added UI text field

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,8 +1,13 @@
 library widgets;
 
+export 'src/ui/UICodeBrew.dart';
 export 'src/ui/widgets/UIPasswordField.dart';
 export 'src/ui/widgets/UIPhoneNumberField.dart';
 export 'src/ui/widgets/UIProfileImage.dart';
 export 'src/ui/widgets/UIDropdown.dart';
 export 'src/ui/widgets/UIPinField.dart';
 export 'src/ui/widgets/UIButton.dart';
+export 'src/ui/widgets/UITextFormField.dart';
+
+// CB Views
+export 'src/ui/modules/CBViewEmailLogin.dart';
